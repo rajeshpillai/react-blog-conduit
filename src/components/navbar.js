b160de97e9c25fb23f6f35ea0e064d047a9a83e7
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-08-26 10:53:32 +0530

    Conduit blog app.  Added basic NavBar

---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-sm navbar-dark bg-primary">
+      <div className="container">
+        <a href="#" className="navbar-brand">
+          BLOG
+        </a>
+      </div>
+    </nav>
+  )
+}
