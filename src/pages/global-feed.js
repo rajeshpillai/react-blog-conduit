3f84684d0220f74c25505ab6bea93a06ae640354
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-08-28 14:43:26 +0530

    Tag

--- src/pages/global-feed.js
+++ src/pages/global-feed.js
@@ -9,6 +9,14 @@ export default function GlobalFeed({data}) {
             <article key={data.slug} className="post-item">
               <h4>{item.title}</h4>
               <div>{item.body}</div>
+
+              <footer className="tags">
+                {
+                  item.tag_list.map(t => {
+                    return <span className="tag">{t}</span>
+                  })
+                }
+              </footer>
             </article>
           )
         })
